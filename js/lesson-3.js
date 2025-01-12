f48908f7254a781d7bb5f6e3a82c439138dea17b
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-01-12 14:26:35 +0100

    add-lesson3.2

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -97,4 +97,15 @@ function getUsersWithEyeColor(users,color){
     return newArray;
 }
 
-console.log(getUsersWithEyeColor(users, 'blue'));
+//console.log(getUsersWithEyeColor(users, 'blue'));
+
+
+// Отримати масив імен користувачів за статтю (поле gender)
+// console.log(getUsersWithGender(users, 'male')); 
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+const getUsersWithGender = (users, gender) => {
+  return users
+   .filter(user => user.gender === gender)
+   .map(user => user.name)
+}
+console.log(getUsersWithGender(users, 'male'))
