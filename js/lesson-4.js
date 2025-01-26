492981dad1d714e568b83b8d44d1cac7e9787940
SvitlanaPro <128604473+SvitlanaPro@users.noreply.github.com>
2025-01-26 15:03:40 +0200

    task-2

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -2,8 +2,24 @@
 Завдання 1
 Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
 */
-const button = document.querySelector("#alertButton");
-const input = document.querySelector("#alertInput")
-button.addEventListener("click", () => {
-    console.log(input.value);
-})
+// const button = document.querySelector("#alertButton");
+// const input = document.querySelector("#alertInput")
+// button.addEventListener("click", () => {
+//     console.log(input.value);
+// })
+/*
+Завдання 2
+Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+*/
+const inputFirst = document.querySelector('#leftSwapInput');
+const inputSecond = document.querySelector('#rightSwapInput');
+const button1 = document.querySelector('#swapButton');
+
+button1.addEventListener('click',funcSwap)
+
+function funcSwap(e){
+    const third = inputFirst.value ;
+    inputFirst.value = inputSecond.value;
+    inputSecond.value = third;
+}
